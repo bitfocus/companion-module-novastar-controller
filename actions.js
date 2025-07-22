4ac053a55ef00169c4cd589397225b733ead442b
John A Knight Jr <istnv@istnv.com>
2025-07-21 21:20:04 -0700

    Initial update for VX-Pro series

--- actions.js
+++ actions.js
@@ -1,4 +1,6 @@
-const nova_config = require('./choices')
+import { EndOfLineState } from 'typescript'
+import * as nova_config from './choices.js'
+import { Regex, InstanceStatus } from '@companion-module/base'
 
 // function to build the byte sequence given the configurable layer options for configuring layers on VX1000
 function getLayerUpdateCommandVX1000(
@@ -79,85 +81,123 @@ function getLayerUpdateCommandVX1000(
 	return commandBuffer
 }
 
-function makeBrightnessCommand(pct,instance) {
-	let val = Math.round(255 * pct / 100)
+function makeBrightnessCommand(pct, instance, which) {
+	let val = Math.round((255 * pct) / 100)
 	let buf = []
 
+	// pre-fill buffer
 	buf.push(
-		Buffer.from([0x55,0xaa,	0x00,	0x00,	0xfe,	0xff,	0x01,	0xff,	0xff,	0xff,	0x01,	0x00,	0x01,	0x00,	0x00,	0x02,	0x01,	0x00,	val,]),
+		//             55:   aa:   00:   07:   fe:   ff:   01:   ff:   ff:   ff:   01:   00:
+		Buffer.from([0x55, 0xaa, 0x00, 0x00, 0xfe, 0xff, 0x01, 0xff, 0xff, 0xff, 0x01, 0x00]),
+		Buffer.from([parseInt('ORGBV'.indexOf(which)) + 1]), // 01:
+		//             00:   00:   02:   01:   00:
+		Buffer.from([0x00, 0x00, 0x02, 0x01, 0x00]),
+		Buffer.from([val]),
 	)
-	//buf.push(val)
 	buf.push(instance.getCommandChecksum(Buffer.concat(buf)))
 
 	return Buffer.concat(buf)
 }
 
 // the instance is passed in from index.js, so that we have access to the main instance for the TCP socket, etc.
-exports.getActions = function (instance) {
+export const getActions = function (instance) {
 	let actions = {}
 
 	// Brightness
 	// VX6s, VX4S, NovaProHD, MCTRL4k, NovaPro UHD, NovaPro UHD Jr , VX1000, VX 600, VX16S
-	if (
-		instance.config.modelID == 'vx4s' ||
-		instance.config.modelID == 'vx6s' ||
-		instance.config.modelID == 'mctrl4k' ||
-		instance.config.modelID == 'novaProHD' ||
-		instance.config.modelID == 'novaProUHD' ||
-		instance.config.modelID == 'novaProUHDJr' ||
-		instance.config.modelID == 'vx1000' ||
-		instance.config.modelID == 'vx600' ||
-		instance.config.modelID == 'vx16s'
-	) {
-		actions['change_brightness'] = {
-			name: 'Change Brightness',
-			options: [
-				{
-					type: 'dropdown',
-					name: 'Brightness',
-					id: 'brightness',
-					default: '0',
-					choices: instance.model.brightness,
-				},
-			],
-			callback: async (event) => {
-				let element = nova_config.CHOICES_BRIGHTNESS.find((element) => element.id === event.options.brightness)
+	// if (
+	// 	instance.config.modelID == 'vx4s' ||
+	// 	instance.config.modelID == 'vx6s' ||
+	// 	instance.config.modelID == 'mctrl4k' ||
+	// 	instance.config.modelID == 'novaProHD' ||
+	// 	instance.config.modelID == 'novaProUHD' ||
+	// 	instance.config.modelID == 'novaProUHDJr' ||
+	// 	instance.config.modelID == 'vx1000' ||
+	// 	instance.config.modelID == 'vx600' ||
+	// 	instance.config.modelID == 'vx16s'
+	// ) {
+	actions['change_brightness'] = {
+		name: 'Change Brightness',
+		options: [
+			{
+				type: 'dropdown',
+				name: 'Brightness',
+				id: 'brightness',
+				default: '0',
+				choices: instance.model.brightness,
+			},
+		],
+		callback: async (event) => {
+			let element = nova_config.CHOICES_BRIGHTNESS.find((element) => element.id === event.options.brightness)
 
-				instance.socket.send(element.cmd)
+			instance.sendMessage(element.cmd)
+		},
+	}
+	actions['set_brightness'] = {
+		name: 'Set Brightness',
+		options: [
+			{
+				type: 'dropdown',
+				label: 'Set / Adjust',
+				id: 'mode',
+				default: 'S',
+				choices: [
+					{ id: 'A', label: 'Adjust +/- Value' },
+					{ id: 'S', label: 'Set Direct Value' },
+				],
 			},
-		}
-		actions['set_brightness'] = {
-			name: 'Set Brightness',
-			options: [
-				{
-					type: 'dropdown',
-					label: 'Set / Adjust',
-					tooltip: 'WIP: Adjust needs feedbacks before\n it can be implemented',
-					id: 'mode',
-					default: 'S',
-					choices: [
-						// { id: 'A', label: 'Adjust +/- Value' },
-						{ id: 'S', label: 'Set Direct Value' },
-					],
-				},
-				{
-					type: 'textinput',
-					label: 'Value (%)',
-					id: 'value',
-					default: 0,
-					useVariables: true,
-				},
-			],
-			callback: async (event, context) => {
-				let val = parseFloat(await context.parseVariablesInString(event.options.value))
-				/*
-					Adjust value based on dynamic variable here
-				*/
-				let cmd = makeBrightnessCommand(val,instance)
-				instance.log('debug',instance.toHexString(cmd));
-				instance.socket.send(cmd)
+			{
+				type: 'dropdown',
+				label: 'Which Color?',
+				id: 'which',
+				default: 'O',
+				choices: [
+					{ id: 'O', label: 'Overall/Global' },
+					{ id: 'R', label: 'Red' },
+					{ id: 'G', label: 'Green' },
+					{ id: 'B', label: 'Blue' },
+					{ id: 'V', label: 'Virtual Red' },
+				],
 			},
-		}
+			{
+				type: 'textinput',
+				label: 'Value (0-100)',
+				id: 'value',
+				isVisible: (options, data) => {
+					return options.mode === 'S'
+				},
+				default: 0,
+				useVariables: true,
+				regex: Regex.FLOAT,
+			},
+
+			{
+				type: 'textinput',
+				label: 'By (+/-) ',
+				id: 'adj',
+				default: 0,
+				isVisible: (options, data) => {
+					return options.mode === 'A'
+				},
+				useVariables: true,
+				regex: Regex.SIGNED_FLOAT,
+			},
+		],
+		callback: async (event, context) => {
+			const val = parseFloat(await context.parseVariablesInString(event.options.value))
+			const aVal = parseFloat(await context.parseVariablesInString(event.options.adj)) || 0
+			const which = event.options.which || 'O'
+			let wb = 'brite' + (which == 'O' ? '' : '_' + which.toLowerCase())
+
+			let newVal = event.options.mode == 'A' ? instance[wb] + aVal : val
+			newVal = Math.max(0, Math.min(100, newVal))
+			instance[wb] = newVal
+			instance.setVariableValues({ [wb]: newVal })
+			let cmd = makeBrightnessCommand(newVal, instance, which)
+			instance.log('debug', instance.toHexString(cmd))
+			instance.sendMessage(cmd)
+		},
+		// }
 	}
 
 	// Change Input
@@ -185,7 +225,7 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = instance.model.inputs.find((element) => element.id === event.options.input)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 	}
@@ -200,13 +240,90 @@ exports.getActions = function (instance) {
 				label: 'Test Patterns',
 				id: 'pattern',
 				default: '0',
+				allowCustom: true,
 				choices: nova_config.CHOICES_TESTPATTERNS,
 			},
 		],
 		callback: async (event) => {
 			let element = nova_config.CHOICES_TESTPATTERNS.find((element) => element.id === event.options.pattern)
 
-			instance.socket.send(element.cmd)
+			instance.sendMessage(element.cmd)
+		},
+	}
+
+	// Select test pattern
+	//
+
+	async function sendTestPatternCmd(event, ctx, list = '') {
+		let which = event.options.pattern
+		// see if it's a custom expression
+		let pat_list = list == 'vx' ? nova_config.CHOICES.TESTPATTERN_VX : nova_config.CHOICES.TESTPATTERN
+
+		let element = pat_list.find((element) => element.id == which)
+
+		if (element == undefined) {
+			which = parseInt(await ctx.parseVariablesInString(event.options.pattern))
+			element = pat_list[which]
+		}
+
+		if (element == undefined) {
+			instance.updateStatus(InstanceStatus.UnknownWarning, `Invalid Test Pattern #${which}`)
+		} else {
+			let msg = Buffer.concat([
+				Buffer.from(instance.bSop), // Start output packet
+				Buffer.from([0, 0, 0xfe]), // ACK, count, source (fe = computer)
+				Buffer.from(element.dest, 'hex'), // message destination, device, address
+				Buffer.from([1, 0]), // 1 (command), 0 (reserved)
+				Buffer.from(element.loc, 'hex'),
+				Buffer.from([0, 0]), // place for 'length' of data
+			])
+			if (element.len > 0) {
+				msg.writeInt16LE(element.len, 16)
+				msg = Buffer.concat([msg, Buffer.from(element.val, 'hex')])
+			}
+			msg = Buffer.concat([msg, instance.getCommandChecksum(msg)]) // add
+
+			instance.sendMessage(msg)
+		}
+	}
+
+	// Test patterns
+	// VX with variables
+	if (instance.config.modelID.slice(0, 2) == 'vx') {
+		actions['test_pattern_vx'] = {
+			name: 'Select Test Pattern (VX Series)',
+			options: [
+				{
+					type: 'dropdown',
+					label: 'Which Pattern?',
+					id: 'pattern',
+					default: nova_config.CHOICES.TESTPATTERN_VX[0].id,
+					allowCustom: true,
+					choices: nova_config.CHOICES.TESTPATTERN_VX,
+				},
+			],
+			callback: async (event, ctx) => {
+				await sendTestPatternCmd(event, ctx, 'vx')
+			},
+		}
+	}
+
+	// Test Patterns
+	// All models with variables
+	actions['test_pattern'] = {
+		name: 'Select Test Pattern',
+		options: [
+			{
+				type: 'dropdown',
+				label: 'Which Pattern?',
+				id: 'pattern',
+				default: nova_config.CHOICES.TESTPATTERN[0].id,
+				allowCustom: true,
+				choices: nova_config.CHOICES.TESTPATTERN,
+			},
+		],
+		callback: async (event, ctx) => {
+			await sendTestPatternCmd(event, ctx, '')
 		},
 	}
 
@@ -226,14 +343,13 @@ exports.getActions = function (instance) {
 		callback: async (event) => {
 			let element = instance.model.displayModes.find((element) => element.id === event.options.display_mode)
 
-			instance.socket.send(element.cmd)
+			instance.sendMessage(element.cmd)
 		},
 	}
 
 	// Working mode
 	// VX6s & J6
 	if (instance.model.workingModes) {
-		// instance.config.modelID == 'vx6s' || instance.config.modelID == 'j6') {
 		actions['change_working_mode'] = {
 			name: 'Change Working Mode',
 			options: [
@@ -248,15 +364,13 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = instance.model.workingModes.find((element) => element.id === event.options.working_mode)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 	}
 
 	// PIP
-	// VX4S, NovaProHD
 	if (instance.model.piponoffs) {
-		// instance.config.modelID == 'vx4s' || instance.config.modelID == 'novaProHD') {
 		actions['pip_onoff'] = {
 			name: 'PIP On/Off',
 			options: [
@@ -271,7 +385,7 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = instance.model.piponoffs.find((element) => element.id === event.options.value)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 	}
@@ -280,7 +394,7 @@ exports.getActions = function (instance) {
 	if (instance.model.layers) {
 		//instance.config.modelID == 'vx1000') {
 		actions['update_layer_vx1000'] = {
-			name: 'Update Layer VX1000',
+			name: 'Update Layer VX',
 			options: [
 				{
 					type: 'dropdown',
@@ -392,14 +506,14 @@ exports.getActions = function (instance) {
 					opacity,
 				)
 
-				instance.socket.send(cmd)
+				instance.sendMessage(cmd)
 			},
 		}
 	}
 
 	// Scaling
-	// VX4S, NovaProHD
-	if (instance.config.modelID == 'vx4s' || instance.config.modelID == 'novaProHD') {
+	//	if (instance.config.modelID == 'vx4s' || instance.config.modelID == 'novaProHD') {
+	if (instance.model.scaling) {
 		actions['change_scaling'] = {
 			name: 'Change Scaling',
 			options: [
@@ -414,21 +528,22 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = nova_config.CHOICES_SCALING.find((element) => element.id === event.options.scale)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 	}
 
 	// LOAD PRESETS
 	// VX4S, NovaPro UHD Jr,vx1000
-	if (
-		instance.config.modelID == 'vx4s' ||
-		instance.config.modelID == 'novaProUHDJr' ||
-		instance.config.modelID == 'vx1000' ||
-		instance.config.modelID == 'vx600' ||
-		instance.config.modelID == 'vx16s' ||
-		instance.config.modelID == 'j6'
-	) {
+	// if (
+	// 	instance.config.modelID == 'vx4s' ||
+	// 	instance.config.modelID == 'novaProUHDJr' ||
+	// 	instance.config.modelID == 'vx1000' ||
+	// 	instance.config.modelID == 'vx600' ||
+	// 	instance.config.modelID == 'vx16s' ||
+	// 	instance.config.modelID == 'j6'
+	// ) {
+	if (instance.model.presets) {
 		actions['load_preset'] = {
 			name: 'Recall Preset',
 			options: [
@@ -443,7 +558,7 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = instance.model.presets.find((element) => element.id === event.options.preset)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 	}
@@ -464,7 +579,7 @@ exports.getActions = function (instance) {
 			callback: async (event) => {
 				let element = instance.model.presets.find((element) => element.id === event.options.preset)
 
-				instance.socket.send(element.cmd)
+				instance.sendMessage(element.cmd)
 			},
 		}
 
@@ -477,7 +592,7 @@ exports.getActions = function (instance) {
 					0x00, 0x95, 0x56,
 				])
 
-				instance.socket.send(cmd)
+				instance.sendMessage(cmd)
 			},
 		}
 	}
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // NovaStar-Controller
-const { InstanceBase, TCPHelper, runEntrypoint, InstanceStatus } = require('@companion-module/base')
-const actions = require('./actions')
-const nova_config = require('./choices')
-const { buffer } = require('stream/consumers')
+import { InstanceBase, TCPHelper, runEntrypoint, InstanceStatus } from '@companion-module/base'
+import * as actions from './actions.js'
+import * as nova_config from './choices.js'
+import { compileVariableDefinitions } from './variables.js'
 
 class NovaStarInstance extends InstanceBase {
 	constructor(internal) {
@@ -11,6 +11,8 @@ class NovaStarInstance extends InstanceBase {
 		// map the choices model to the internal config data object
 		this.CHOICES_MODEL = Object.values(nova_config.CONFIG_MODEL)
 
+		this.bAck = Buffer.from([0xaa, 0x55])
+		this.bSop = Buffer.from([0x55, 0xaa])
 		// Sort config model alphabetically
 		this.CHOICES_MODEL.sort(function (a, b) {
 			let x = a.label.toLowerCase()
@@ -23,18 +25,67 @@ class NovaStarInstance extends InstanceBase {
 			}
 			return 0
 		})
+
+		this.waiting = []
+		this.msgCounter = 0
 	}
 
-	toHexString(msg) {
+	/**
+	 * returns a hex string representation
+	 *
+	 * @param {buffer} msg - source message
+	 * @param {string} [sep=''] - optional seperator
+	 * @returns {string}
+	 */
+	toHexString(msg, sep = '') {
 		let resp = ''
 		for (var i = 0; i < msg.length; i++) {
-			resp += ('0' + msg[i].toString(16)).slice(-2) + ':'
+			resp += ('0' + msg[i].toString(16)).slice(-2) + sep
 		}
 		return resp
 	}
 
+	/**
+	 * return the hex digits of the message destination (address+device type+port address + board address LSB+MSB)
+	 *
+	 * @param {buffer} msg - source message
+	 *
+	 * @returns {string}
+	 */
+	msgDevice(msg) {
+		return this.toHexString(msg.subarray(5, 9))
+	}
+
+	/**
+	 * return the hex digits of the paramater address (including offset)
+	 * @param {buffer} msg - source message
+	 *
+	 * @returns {string}
+	 */
+	msgLocation(msg) {
+		return this.toHexString(msg.subarray(12, 16), ':').split(':').reverse().join('')
+	}
+
+	/**
+	 * return the hex digits of the paramater address, offset, and data length
+	 *
+	 * @param {buffer} msg - source message
+	 *
+	 * @returns {string}
+	 */
+	msgData(msg) {
+		const dataLen = msg.readInt16LE(16)
+
+		return dataLen == 0 ? '' : this.toHexString(msg.subarray(18, 18 + dataLen))
+	}
+
 	updateActions() {
 		this.setActionDefinitions(actions.getActions(this))
+	}
+
+	init_variables() {
+		this.variableDefs = compileVariableDefinitions(this)
+		this.setVariableDefinitions(this.variableDefs)
 	}
 
 	// Return config fields for web config
@@ -82,17 +133,15 @@ class NovaStarInstance extends InstanceBase {
 		if (this.config.modelID !== undefined) {
 			this.model = nova_config.CONFIG_MODEL[this.config.modelID]
 		}
-		// else {
-		// 	this.config.modelID = 'vx4s'
-		// 	this.model = nova_config.CONFIG_MODEL['vx4s']
-		// }
 
 		// this is not called by Companion directly, so we need to call this to load the actions into Companion
+		this.init_variables()
 		this.updateActions()
 
 		// start up the TCP socket and attmept to get connected to the NovaStar device
 		this.initTCP()
 	}
+
 	// calculates the checksum for dynamic commands
 	getCommandChecksum(pipCommandBuffer) {
 		// we can't include the first two bytes in our checksum calculation, so we are taking a subarray excluding the first two bytes
@@ -110,12 +159,14 @@ class NovaStarInstance extends InstanceBase {
 
 		// split the sum into two bytes in little endian, this will be placed on the end of our command to serve as the checksum
 		const resultChecksumBuffer = Buffer.allocUnsafe(2)
-		resultChecksumBuffer.writeInt16LE(sum)
+		resultChecksumBuffer.writeUInt16LE(sum)
 
 		return resultChecksumBuffer
 	}
 
 	initTCP() {
+		let receivebuffer = Buffer.from('')
+
 		if (this.socket !== undefined) {
 			// clean up the socket and keep Companion connection status up to date
 			this.socket.destroy()
@@ -126,6 +177,10 @@ class NovaStarInstance extends InstanceBase {
 		if (this.config.port === undefined) {
 			// use TCP port 5200 by default
 			this.config.port = 5200
+		}
+
+		if (this.config.modelID == 'vxpro') {
+			this.config.port = 15200
 		}
 
 		if (this.config.host) {
@@ -145,46 +200,168 @@ class NovaStarInstance extends InstanceBase {
 				this.updateStatus(InstanceStatus.ConnectionFailure, `Cannot connect to ${this.config.host}`)
 			})
 
-			this.socket.on('connect', () => {
-				// extract the 'Brightness' from the 1st sending card
-				// let cmd = []
-				// for (const p of nova_config.CHOICES_DISPLAYMODE_MCTRL) {
-				// 	cmd = p.cmd.subarray(0, p.cmd.length - 2)
+			this.socket.on('connect', async () => {
+				// Get the model of the sending card/processor
+				let data
 
-				// 	// cmd.push(
-				// 	// 	Buffer.from([
-				// 	// 		0x55,
-				// 	// 		0xaa, // header
-				// 	// 		0x00, // ack (unused)
-				// 	// 		0x10 + p, // serial counter
-				// 	// 		0xfe, // source address
-				// 	// 		0x00, // destination address
-				// 	// 		0x01, // device type
-				// 	// 		p, // port address
-				// 	// 		0x00,
-				// 	// 		0x00, // board address
-				// 	// 		0x00, // code
-				// 	// 		0x00, // reserved
-				// 	// 		0x01,
-				// 	// 		0x00,
-				// 	// 		0x00,
-				// 	// 		0x02, // register address
-				// 	// 		0x05,
-				// 	// 		0x00, // data length
-				// 	// 	]),
-				// 	// )
-				// 	cmd = Buffer.concat([cmd, this.getCommandChecksum(cmd)])
-				// 	this.log('debug', this.toHexString(cmd))
-				// 	//this.socket.send(Buffer.concat(cmd))
-				// }
+				let cmd = Buffer.from([
+					0x55,
+					0xaa, // header
+					0x00, // ack (unused)
+					0x00, // serial counter
+					0xfe, // source address
+					0x00, // destination address
+					0x00, // device type
+					0x00, // port address
+					0x00,
+					0x00, // board address
+					0x00, // code
+					0x00, // reserved
+					0x02,
+					0x00,
+					0x00,
+					0x00, // register address
+					0x02,
+					0x00, // data length
+				])
+
+				// get sending device model id
+				let msg = Buffer.concat([cmd, this.getCommandChecksum(cmd)])
+
+				this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				data = await this.sendMessage(msg)
+				this.log('debug', `Controller ID = ${data}`)
+				this.setVariableValues({ ctrl_id: data })
+				this.setVariableValues({ ctrl_type: nova_config.MODEL_IDS[data]?.label || 'Unknown' })
+
+				// get first receiving card model id
+				msg[6] = 1
+				this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				data = await this.sendMessage(msg)
+				this.log('debug', `Receiver ID = ${data}`)
+
+				// get DVI Status
+				msg[6] = 0
+				msg[12] = 0x17
+				msg[15] = 2
+				msg[16] = 1
+				this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				data = await this.sendMessage(msg)
+				this.log('debug', `DVI Status = ${data}`)
+
+				// sending card firmware version
+				cmd[6] = 0
+				cmd[12] = 4
+				cmd[13] = 0
+				cmd[14] = 0x10
+				cmd[15] = 0x04
+				cmd[16] = 0x04
+				msg = Buffer.concat([cmd, this.getCommandChecksum(cmd)])
+				this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				data = await this.sendMessage(msg)
+				this.log('debug', `Controller FW = ${data}`)
+				this.setVariableValues({
+					ctrl_fw: `${parseInt(data.slice(0, 2), 16)}.${parseInt(data.slice(2, 4), 16)}.${parseInt(data.slice(4, 6), 16)}.${parseInt(data.slice(6, 8), 16)}`,
+				})
+
+				// // get first receiving card FW version
+				// msg[6] = 1
+				// this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				// data = await this.sendMessage(msg)
+				// this.log('debug', `Receiver fw = ${data}`)
+				// this.setVariableValues({ led_fw: `${parseInt(data.slice(0, 2), 16)}.${parseInt(data.slice(2, 4), 16)}.${parseInt(data.slice(4, 6), 16)}.${parseInt(data.slice(6, 8), 16)}`, })
+
+				// brightness from 1st panel
+				cmd[6] = 1
+				cmd[12] = 1
+				cmd[13] = 0
+				cmd[14] = 0
+				cmd[15] = 2
+				cmd[16] = 5
+
+				msg = Buffer.concat([cmd, this.getCommandChecksum(cmd)])
+				this.log('debug', `==> ${this.toHexString(msg, ':')}`)
+				data = await this.sendMessage(msg)
+				this.log('debug', `Brightness = ${data}`)
+				this.brite = Math.round(((parseInt(data.slice(0, 2), 16) * 100) / 255) * 2) / 2
+				this.setVariableValues({ brite: this.brite })
+
+				cmd = Buffer.from(nova_config.CHOICES_DISPLAYMODE_MCTRL[0].cmd.subarray(0, 20))
+				for (let p = 7; p < 11; p++) {
+					cmd[p] = 0
+				}
+				data = await this.sendMessage(cmd)
+				this.log('debug', `Display Mode = ${data}`)
+
 				this.log('debug', 'Connected')
 				this.updateStatus(InstanceStatus.Ok, 'Connected')
 			})
 
-			this.socket.on('data', (msg) => {
-				this.log('debug', this.toHexString(msg) + '\n')
+			this.socket.on('data', (chunk) => {
+				// separate buffered stream into parsed messages
+				let i = 0,
+					msg = Buffer.from(''),
+					offset = 0,
+					dataLen = 0,
+					calcChk = []
+				receivebuffer = Buffer.concat([receivebuffer, chunk])
+				while (receivebuffer.length > 19)
+					if (receivebuffer[0] != this.bAck[0]) {
+						receivebuffer = receivebuffer.subarray(1)
+					} else if (receivebuffer.length > 19 && receivebuffer[1] == this.bAck[1]) {
+						// check for any data
+						dataLen = receivebuffer.readInt16LE(16)
+						offset = 20 + dataLen
+						if (receivebuffer.length >= offset) {
+							// so far, length is OK
+							// now verify the checksum
+							msg = receivebuffer.subarray(0, offset - 2)
+							calcChk = this.getCommandChecksum(msg)
+							if (receivebuffer.readInt16LE(offset - 2) == calcChk.readInt16LE(0)) {
+								const msgId = msg[3]
+								// message is 'good'
+								this.log('debug', `emit 'response${msgId}' ${this.toHexString(receivebuffer.subarray(0, offset), ':')}`)
+
+								if (!this.socket?.emit(`response${msgId}`, msg)) {
+									this.log(
+										'debug',
+										`       No listener: Loc ${this.msgLocation(msg)} Len ${msg.readInt16LE(16)}\n\t\tData ${this.msgData(msg)}\n`,
+									)
+								}
+							}
+							receivebuffer = receivebuffer.subarray(offset)
+						}
+					}
 			})
+
+			// this.socket.on('response', (msg) => {
+			// 	this.log('debug', `<== ${this.toHexString(msg, ':')}\n`)
+			// 	this.log('debug', `    # ${msg[3]} Loc ${this.msgLocation(msg)} Data ${this.msgData(msg)}\n`)
+			// })
 		}
+	}
+
+	// assemble command and send it
+	async sendMessage(cmd) {
+		return new Promise(async (resolve) => {
+			let msg = Buffer.from(cmd.subarray(0, cmd.length - 2))
+			let isQuery = msg[10] == 0
+			let msgId = this.msgCounter++ % 256
+			let data = ''
+			msg[3] = msgId
+			msg = Buffer.concat([msg, this.getCommandChecksum(msg)])
+			this.log('debug', `==> ${isQuery ? ' request ' : ''}${this.toHexString(msg, ':')}`)
+			await this.socket.send(msg)
+			this.socket.once(`response${msgId}`, async (msg) => {
+				data = this.msgData(msg)
+				this.log('debug', `<== ${this.toHexString(msg, ':')}\n`)
+				this.log('debug', `    # ${msg[3]} Loc ${this.msgLocation(msg)} Data ${data}\n`)
+				this.updateStatus(InstanceStatus.Ok, '')
+				if (isQuery) {
+					resolve(data)
+				}
+			})
+		})
 	}
 
 	async configUpdated(config) {
